68d4388465fa4186a33e4ff74314286d96e81843
Zaargh <sailor88@gmail.com>
2016-02-25 11:12:15 +0100

    OCR.space example

---
+++ ocrspace_example.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def ocr_space(filename, overlay=False, api_key='helloworld', language='en'):
+    """ OCR.space API request
+        Python3.5 - not tested on 2.7
+    :param filename: Your file path & name.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+
+    payload = {'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    with open(filename, 'rb') as f:
+        r = requests.post('https://api.ocr.space/parse/image',
+                          files={filename: f},
+                          data=payload,
+                          )
+    return r.content.decode()
+
+
+# Use example:
+test = ocr_space('example_image.png', language='pol')
