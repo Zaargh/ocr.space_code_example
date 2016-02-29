3cf2a54abce6ee889977eaec9762c5cda5ec0df3
Zaargh <sailor88@gmail.com>
2016-02-29 09:03:12 +0100

    Added url functionality, fixed for change in language code in api (en->eng)

--- ocrspace_example.py
+++ ocrspace_example.py
@@ -1,8 +1,8 @@
 import requests
 
 
-def ocr_space(filename, overlay=False, api_key='helloworld', language='en'):
-    """ OCR.space API request
+def ocr_space_file(filename, overlay=False, api_key='helloworld', language='eng'):
+    """ OCR.space API request with local file.
         Python3.5 - not tested on 2.7
     :param filename: Your file path & name.
     :param overlay: Is OCR.space overlay required in your response.
@@ -27,5 +27,31 @@ def ocr_space(filename, overlay=False, api_key='helloworld', language='en'):
     return r.content.decode()
 
 
-# Use example:
-test = ocr_space('example_image.png', language='pol')
+def ocr_space_url(url, overlay=False, api_key='helloworld', language='eng'):
+    """ OCR.space API request with remote file.
+        Python3.5 - not tested on 2.7
+    :param url: Image url.
+    :param overlay: Is OCR.space overlay required in your response.
+                    Defaults to False.
+    :param api_key: OCR.space API key.
+                    Defaults to 'helloworld'.
+    :param language: Language code to be used in OCR.
+                    List of available language codes can be found on https://ocr.space/OCRAPI
+                    Defaults to 'en'.
+    :return: Result in JSON format.
+    """
+
+    payload = {'url': url,
+               'isOverlayRequired': overlay,
+               'apikey': api_key,
+               'language': language,
+               }
+    r = requests.post('https://api.ocr.space/parse/image',
+                      data=payload,
+                      )
+    return r.content.decode()
+
+
+# Use examples:
+test_file = ocr_space_file(filename='example_image.png', language='pol')
+test_url = ocr_space_url(url='http://i.imgur.com/31d5L5y.jpg')
